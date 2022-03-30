e4ce1b901481d5c8eefb0a0bf6cee8caf5b64c49
Marius Kleppe Larnøy <mariusklarnoy@gmail.com>
2022-03-30 16:57:52 +0200

    cuda setup

--- examples/pde/cpp-src/gen/examples/pde/mg-src/pde-cpp.cuh
+++ examples/pde/cpp-src/gen/examples/pde/mg-src/pde-cpp.cuh
@@ -11,7 +11,7 @@ namespace pde_cpp {
 struct PDEProgram {
     struct _two {
         template <typename T>
-        inlineT operator()() {
+        inline T operator()() {
             T o;
             PDEProgram::two0(o);
             return o;
@@ -21,7 +21,7 @@ struct PDEProgram {
     static PDEProgram::_two two;
     struct _one {
         template <typename T>
-        inlineT operator()() {
+        inline T operator()() {
             T o;
             PDEProgram::one0(o);
             return o;
@@ -34,7 +34,7 @@ private:
 public:
     typedef array_ops::Offset Offset;
 private:
-    static inlinevoid one0(PDEProgram::Offset& o) {
+    static inline void one0(PDEProgram::Offset& o) {
         o = __array_ops.one_offset();
     };
 public:
@@ -42,140 +42,140 @@ public:
     typedef array_ops::Index Index;
     typedef array_ops::Float Float;
     struct _three {
-        inlinePDEProgram::Float operator()() {
+        inline PDEProgram::Float operator()() {
             return __array_ops.three_float();
         };
     };
 
     static PDEProgram::_three three;
     struct _unary_sub {
-        inlinePDEProgram::Float operator()(const PDEProgram::Float& f) {
+        inline PDEProgram::Float operator()(const PDEProgram::Float& f) {
             return __array_ops.unary_sub(f);
         };
-        inlinePDEProgram::Offset operator()(const PDEProgram::Offset& o) {
+        inline PDEProgram::Offset operator()(const PDEProgram::Offset& o) {
             return __array_ops.unary_sub(o);
         };
     };
 
     static PDEProgram::_unary_sub unary_sub;
 private:
-    static inlinevoid one0(PDEProgram::Float& o) {
+    static inline void one0(PDEProgram::Float& o) {
         o = __array_ops.one_float();
     };
-    static inlinevoid two0(PDEProgram::Float& o) {
+    static inline void two0(PDEProgram::Float& o) {
         o = __array_ops.two_float();
     };
 public:
     typedef array_ops::Axis Axis;
     struct _rotate_ix {
-        inlinePDEProgram::Index operator()(const PDEProgram::Index& ix, const PDEProgram::Axis& axis, const PDEProgram::Offset& o) {
+        inline PDEProgram::Index operator()(const PDEProgram::Index& ix, const PDEProgram::Axis& axis, const PDEProgram::Offset& o) {
             return __array_ops.rotate_ix(ix, axis, o);
         };
     };
 
     static PDEProgram::_rotate_ix rotate_ix;
     struct _zero {
-        inlinePDEProgram::Axis operator()() {
+        inline PDEProgram::Axis operator()() {
             return __array_ops.zero_axis();
         };
     };
 
     static PDEProgram::_zero zero;
 private:
-    static inlinevoid one0(PDEProgram::Axis& o) {
+    static inline void one0(PDEProgram::Axis& o) {
         o = __array_ops.one_axis();
     };
-    static inlinevoid two0(PDEProgram::Axis& o) {
+    static inline void two0(PDEProgram::Axis& o) {
         o = __array_ops.two_axis();
     };
 public:
     typedef array_ops::Array Array;
     struct _binary_add {
-        inlinePDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
+        inline PDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
             return __array_ops.binary_add(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.binary_add(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.binary_add(lhs, rhs);
         };
     };
 
     static PDEProgram::_binary_add binary_add;
     struct _binary_sub {
-        inlinePDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
+        inline PDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
             return __array_ops.binary_sub(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.binary_sub(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.binary_sub(lhs, rhs);
         };
     };
 
     static PDEProgram::_binary_sub binary_sub;
     struct _div {
-        inlinePDEProgram::Float operator()(const PDEProgram::Float& num, const PDEProgram::Float& den) {
+        inline PDEProgram::Float operator()(const PDEProgram::Float& num, const PDEProgram::Float& den) {
             return __array_ops.div(num, den);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Float& num, const PDEProgram::Array& den) {
+        inline PDEProgram::Array operator()(const PDEProgram::Float& num, const PDEProgram::Array& den) {
             return __array_ops.div(num, den);
         };
     };
 
     static PDEProgram::_div div;
     struct _forall_ix_snippet {
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
             return __forall_ops.forall_ix_snippet(u, v, u0, u1, u2, c0, c1, c2, c3, c4);
         };
     };
 
     static PDEProgram::_forall_ix_snippet forall_ix_snippet;
     struct _forall_ix_snippet_cuda {
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
             return __forall_ops.forall_ix_snippet_cuda(u, v, u0, u1, u2, c0, c1, c2, c3, c4);
         };
     };
 
     static PDEProgram::_forall_ix_snippet_cuda forall_ix_snippet_cuda;
     struct _mul {
-        inlinePDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
+        inline PDEProgram::Float operator()(const PDEProgram::Float& lhs, const PDEProgram::Float& rhs) {
             return __array_ops.mul(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Float& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.mul(lhs, rhs);
         };
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& lhs, const PDEProgram::Array& rhs) {
             return __array_ops.mul(lhs, rhs);
         };
     };
 
     static PDEProgram::_mul mul;
     struct _psi {
-        inlinePDEProgram::Float operator()(const PDEProgram::Index& ix, const PDEProgram::Array& array) {
+        inline PDEProgram::Float operator()(const PDEProgram::Index& ix, const PDEProgram::Array& array) {
             return __array_ops.psi(ix, array);
         };
     };
 
     static PDEProgram::_psi psi;
     struct _rotate {
-        inlinePDEProgram::Array operator()(const PDEProgram::Array& a, const PDEProgram::Axis& axis, const PDEProgram::Offset& o) {
+        inline PDEProgram::Array operator()(const PDEProgram::Array& a, const PDEProgram::Axis& axis, const PDEProgram::Offset& o) {
             return __array_ops.rotate(a, axis, o);
         };
     };
 
     static PDEProgram::_rotate rotate;
     struct _snippet {
-        inlinevoid operator()(PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
+        inline void operator()(PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4) {
             u = PDEProgram::forall_ix_snippet(u, v, u0, u1, u2, c0, c1, c2, c3, c4);
         };
     };
 
     static PDEProgram::_snippet snippet;
     struct _snippet_ix {
-        inlinePDEProgram::Float operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4, const PDEProgram::Index& ix) {
+        inline PDEProgram::Float operator()(const PDEProgram::Array& u, const PDEProgram::Array& v, const PDEProgram::Array& u0, const PDEProgram::Array& u1, const PDEProgram::Array& u2, const PDEProgram::Float& c0, const PDEProgram::Float& c1, const PDEProgram::Float& c2, const PDEProgram::Float& c3, const PDEProgram::Float& c4, const PDEProgram::Index& ix) {
             PDEProgram::Axis zero = PDEProgram::zero();
             PDEProgram::Offset one = PDEProgram::one.operator()<Offset>();
             PDEProgram::Axis two = PDEProgram::two.operator()<Axis>();
@@ -189,7 +189,7 @@ private:
 public:
     static PDEProgram::_snippet_ix snippet_ix;
     struct _step {
-        inlinevoid operator()(PDEProgram::Array& u0, PDEProgram::Array& u1, PDEProgram::Array& u2, const PDEProgram::Float& nu, const PDEProgram::Float& dx, const PDEProgram::Float& dt) {
+        inline void operator()(PDEProgram::Array& u0, PDEProgram::Array& u1, PDEProgram::Array& u2, const PDEProgram::Float& nu, const PDEProgram::Float& dx, const PDEProgram::Float& dt) {
             PDEProgram::Float one = PDEProgram::one.operator()<Float>();
             PDEProgram::Float _2 = PDEProgram::two.operator()<Float>();
             PDEProgram::Float c0 = PDEProgram::div(PDEProgram::div(one, _2), dx);
--- src/lib/Cxx/Syntax.hs
+++ src/lib/Cxx/Syntax.hs
@@ -607,7 +607,7 @@ prettyCxxFnDef cxxOutMode
   (if isTemplated then pTemplateParams <> line else "") <>
   (if cxxModuleMemberType == CxxStaticMember && cxxOutMode == CxxHeader
    then "static " else "") <>
-  (if isInline then "inline" else "") <> p retTy <+> p name <> "(" <>
+  (if isInline then "inline " else "") <> p retTy <+> p name <> "(" <>
   hsep (punctuate comma (map p params)) <> ")" <>
   case cxxOutMode of
     CxxHeader -> if isTemplated || isInline
